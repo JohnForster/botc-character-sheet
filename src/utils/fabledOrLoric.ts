0b34071455411ae1421ca3b442f57312daccf141
John Forster <john.forster@makers.tech>
2025-11-18 16:13:53 +0000

    Switch fabled/loric and add css rule

--- src/utils/fabledOrLoric.ts
+++ src/utils/fabledOrLoric.ts
@@ -9,16 +9,17 @@ export function getFabledOrLoric(
   const loric = characters.filter((char) => char.team === "loric");
 
   const output = [
+    ...loric.map((lo) => ({
+      name: lo.name,
+      note: lo.ability,
+      image:
+        lo.wiki_image ?? (Array.isArray(lo.image) ? lo.image[0] : lo.image),
+    })),
     ...fabled.map((fb) => ({
       name: fb.name,
       note: fb.ability,
       image:
         fb.wiki_image ?? (Array.isArray(fb.image) ? fb.image[0] : fb.image),
-    })),
-    ...loric.map((lo) => ({
-      name: lo.name,
-      note: lo.ability,
-      image: Array.isArray(lo.image) ? lo.image[0] : lo.image,
     })),
   ];
 
